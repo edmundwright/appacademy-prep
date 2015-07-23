a69ce114780caf3afe984601d4789416e0f47e78
Edmund Wright <edmund.wright@gmail.com>
2015-07-23 11:07:22 -0400

    Do w3d3

--- w3/w3d3/lib/battleship.rb
+++ w3/w3d3/lib/battleship.rb
@@ -1,3 +1,47 @@
+require_relative "board"
+require_relative "player"
+
 class BattleshipGame
   attr_reader :board, :player
+
+  def initialize(player = HumanPlayer.new, board = Board.new)
+    @player = player
+    @board = board
+    board.populate_grid
+  end
+
+  def attack(pos)
+    if board[*pos] == :s
+      board[*pos] = :o
+    else
+      board[*pos] = :x
+    end
+  end
+
+  def count
+    board.count
+  end
+
+  def game_over?
+    board.won?
+  end
+
+  def play_turn
+    attack(player.get_play(board))
+  end
+
+  def display_status
+    board.display
+    puts "Ships remaining: #{board.count}"
+  end
+
+  def play
+    until game_over?
+      display_status
+      play_turn
+    end
+
+    display_status
+    puts "Game over!"
+  end
 end
